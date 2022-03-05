773ed9d94cecc5fb8426ba2ce137cced20e2f9a4
Matthew King <78060961+mattbryanking@users.noreply.github.com>
2022-03-05 14:37:26 -0500

    added functioning but ugly GUI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as sg
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +12,29 @@ url = "https://jklm.fun/"
 
 
 def main():
+    sg.theme('Default1')
+    layout = [
+        [sg.Text('BombParty Room Code:')],
+        [sg.Text('', size=(0, 0)), sg.InputText()],
+        [sg.Text('Select Word Type:')],
+        [sg.Radio('All', "RADIO1", default=True)],
+        [sg.Radio('Impressive', "RADIO1", default=False)],
+        [sg.Radio('Simple', "RADIO1", default=False)],
+        [sg.Submit(), sg.Cancel()]
+    ]
+    window = sg.Window('BombParty Bot 1.0', layout, element_justification='c')
+    event, values = window.read()
+    window.close()
+
+    words_type = None
+    room_number = values[0]
+
+    if values[1]:
+        words_type = 1
+    elif values[2]:
+        words_type = 2
+    else:
+        words_type = 3
     driver = webdriver.Chrome(executable_path=PATH)
     Game.ConnectRoom(driver)
     iframe = 0
