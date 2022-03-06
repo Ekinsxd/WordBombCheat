446c9d2f21aacb4e2720d74cc0ec8d9e9563c1f9
Matthew King <78060961+mattbryanking@users.noreply.github.com>
2022-03-06 00:51:05 -0500

    add full gui control

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import Game
-import os
 
 
 class Main:
